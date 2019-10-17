e88206370f7754932e813570ed6e5b8881408e3f
ChenpinonHuang <ChenpinonHuang@users.noreply.github.com>
2019-10-17 12:43:44 -0400

    Add project files.

---
+++ ASPAssignment/Models/Class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPAssignment.Models
+{
+    public class Class
+    {
+        [Key]
+        public int AlbumId { get; set; }
+    }
+}
