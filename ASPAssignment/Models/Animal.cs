d738c40522f8e013b0589394c827e5d0e7c5f053
ChenpinonHuang <ChenpinonHuang@users.noreply.github.com>
2019-10-17 15:54:35 -0400

    Create the classes for the application

---
+++ ASPAssignment/Models/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPAssignment.Models
+{
+    public class Animal
+    {
+        [Key]
+        public int AnimalId { get; set; }
+        public string Type { get; set; }
+        public string Source { get; set; }
+        public string Feture { get; set; }
+
+        public List<TerrestrialAnimal> TerrestrialAnimal { get; set; }
+    }
+
+}
